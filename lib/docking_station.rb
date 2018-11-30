6022a76888e8e8a7d64472527e55124ce27047ff
OSSahota <osstechunlimited@gmail.com>
2018-11-30 16:37:29 +0000

    15&16 refactoring(srp) and using constants

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,7 @@
 require_relative 'bike'
 class DockingStation
   attr_accessor :bike
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
@@ -9,13 +10,18 @@ class DockingStation
 
   # creates a new bike
   def dock(bike)
-     fail 'Station Full' if @bikes.count >= 20
+     fail 'Station Full' if full?
      @bikes << bike
   end
 
   def release_bike
     fail 'No bikes available' if @bikes.empty?
     @bikes.pop
+  end
+
+  private
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
 
